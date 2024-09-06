373faed004e2525920efd8b6b5e2722db41dca23
CreativeMan <scottking2113@gmail.com>
2024-09-06 17:18:07 +0900

    addition of todo page and install tailwindcss

---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>To Do List</title>
+	<meta name="description" content="To Do List" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>To Do List</h1>
+
+	<p>
+		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app.
+	</p>
+
+	<p>
+		I am going to start to work for To Do List on this page.
+	</p>
+</div>
